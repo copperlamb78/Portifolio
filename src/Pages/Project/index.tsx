5ceb2a7c0d3274195c35e14722a6f9e5fba45bc3
Copperlamb78 <nascimentogabriel.2004@gmail.com>
2024-09-25 18:55:38 -0300

    add first project

--- src/Pages/Project/index.tsx
+++ src/Pages/Project/index.tsx
@@ -1,5 +1,22 @@
+import { Image, LinkDiv, ProjectContainer, ProjectMainContainer, StyledLink } from "./styles";
+import HOME from '../../assets/FirstProject1.png'
+import ADDRES from '../../assets/FirstProject2.png'
+
 export function Project() {
     return(
-        <h1>Project</h1>
+        <ProjectMainContainer>
+            <ProjectContainer>
+                <Image src={HOME}/>
+                <Image src={ADDRES}/>
+                <LinkDiv>
+                    <StyledLink 
+                        href="https://github.com/copperlamb78/Eletronic_Recycling"
+                        target="_blank"
+                    >
+                        Projeto completo
+                    </StyledLink>
+                </LinkDiv>
+            </ProjectContainer>
+        </ProjectMainContainer>
     )
 }
---
+++ src/Pages/Project/styles.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const ProjectMainContainer = styled.div`
+    max-width: 80vw;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5vh;
+    padding: 1vw;
+`
+export const ProjectContainer = styled.div`
+    width: 80vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1vh;
+`
+export const Image = styled.img`
+    width: 30vw;
+    border-radius: 1vw;
+`
+export const LinkDiv = styled.div`
+    
+    /* background-color: ${props => props.theme.colors.primary}; */
+
+
+`
+export const StyledLink = styled.a`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    width: 12vw; 
+    border-radius: 2vw;
+    height: 4vw;
+    background-color: ${props => props.theme.colors.primary};
+    color: ${props => props.theme.colors.bw};
+    transition: background-color 0.3s;
+
+    &:hover {
+        background-color: ${props => props.theme.colors.secondary};
+    }
+`
